39e442a10ee490c6884fc79f696acc78da8a7457
narawit rattanasuriya <narawit@sirisoft.co.th>
2021-07-09 15:30:31 +0700

    add

---
+++ src/main/java/org.demo.greeting/GreetingController.java
@@ -0,0 +1,15 @@
+package org.demo.greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+public class GreetingController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World !!";
+    }
+}
