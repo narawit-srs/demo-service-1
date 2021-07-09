628586fa99dc32d15484b6e2ad8a56520907f493
narawit rattanasuriya <narawit@sirisoft.co.th>
2021-07-09 15:54:50 +0700

    add

--- src/main/java/demoservice/DemoServiceApplication.java
+++ src/main/java/demoservice/DemoServiceApplication.java
@@ -5,14 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @EnableAutoConfiguration
 public class DemoServiceApplication {
 
+	@Value("${USERHELLO}")
+		String user_hello;
+
     @RequestMapping("/")
     String index() {
-        return "Hello World!";
+        return "Hellow "+ user_hello;
     }
 
     public static void main(String[] args) {
