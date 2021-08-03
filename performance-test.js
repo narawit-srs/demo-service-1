f4ffd7f1e913ac6cb4dccf3285f98159ed86adbd
narawit rattanasuriya <narawit@sirisoft.co.th>
2021-08-03 10:12:17 +0700

    x

--- performance-test.js
+++ performance-test.js
@@ -1,12 +1,18 @@
 import { sleep } from 'k6';
 import http from 'k6/http';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export let options = {
   duration: '1m',
   vus: 50,
+  jsonFile: ".",
+  output: ".",
 };
 
 export default function () {
   http.get('http://172.16.16.151:8080/employee');
   sleep(3);
+  return {
+    "summary.html": htmlReport(data),
+  };
 }
